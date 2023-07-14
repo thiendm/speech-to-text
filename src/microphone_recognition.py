33b930911a7803490866da5559ab8a5b94f3c454
dmthien <dmthien@dmtsolutions.com>
2023-07-14 19:03:00 +0700

    adding src code and requirement packages

---
+++ src/microphone_recognition.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recongnize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+# please see more examples at the below link
+# https://github.com/Uberi/speech_recognition/blob/master/examples/microphone_recognition.py
